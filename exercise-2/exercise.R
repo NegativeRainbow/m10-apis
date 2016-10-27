cec2df3e0aae69e9218d4f6cea34c2938aa26d24
NegativeRainbow <danishb@uw.edu>
2016-10-27 14:44:43 -0700

    Did part of exercise 5

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -6,13 +6,21 @@
 library(jsonlite)
 setwd('~/Documents/info-201/m10-apis/exercise-2/')
 
-
 # Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
+topTen <- function(artist, country){
+  if(missing(country)){
+    country = "US"
+  }
+  artist <- gsub(" ", "%20", artist, fixed = TRUE)
+  data <- fromJSON(paste0('https://api.spotify.com/v1/search?q=', artist, '&type=artist'))
+  id <- data$artists$items$id[1]
+  top.tracks <- fromJSON(paste0('https://api.spotify.com/v1/artists/', id, '/top-tracks?country=', country))$tracks
+  return(top.tracks)
+}
 
-
-
+top.nelly.tracks <- topTen('Nelly')
 # What are the top 10 tracks by Nelly?
-
+View(topTen('Avenged Sevenfold'))
 
 
 
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -1,30 +1,31 @@
-### Exercise 5 ###
+git### Exercise 5 ###
 library(jsonlite)
 library(dplyr)
 
 # Write a function that allows you to specify a movie, then does the following:
-  
+movieSummary <- function(movie){
+  movie <- "Kubo and the Two Strings"
   # Replace all of the spaces in your movie title with plus signs (+)
-  
+  movie.no.spaces <- gsub(" ", "+", movie)
   
   # Construct a search query using YOUR api key
-  # The base URL is https://api.nytimes.com/svc/movies/v2/reviews/search.json?
+  baseURL <- "https://api.nytimes.com/svc/movies/v2/reviews/search.json?"
+  api.key <- "&api-key=42a4649b3ebb4dc48059a5804d3d6768"
   # See the interactive console for more detail:https://developer.nytimes.com/movie_reviews_v2.json#/Console/GET/reviews/search.json
-  
+  request <- paste0(baseURL, "?query=", movie.no.spaces, api.key)
   
   # Request data using your search query
-  
-  
+  data <- fromJSON(request)$results
   # What type of variable does this return?
   
   # Flatten the data stored in the `$results` key of the data returned to you
-  
+  data <- flatten(data)
   
   # From the most recent review, store the headline, short summary, and link to full article each in their own variables
   
   
   # Return an list of the three pieces of information from above
   
-
+}
 # Test that your function works with a movie of your choice
 
